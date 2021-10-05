75c95dfd34428d1b49b66782207afe5e251fa4b1
DAlejandroB <diegoaenciso76@gmail.com>
2021-10-05 18:18:00 -0500

    cors module added

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ const exec = require('child_process').exec;
 const readLastLines = require('read-last-lines');
 const axios = require('axios');
 const app = express();
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-const ip = "172.17.0."
+const ip = "172.17.0.";
 
 const port = process.env.PORT || 4001;
 const id = port - 4000;
